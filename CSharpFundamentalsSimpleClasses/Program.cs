ed3f3fccf66ad2408a6b7264facda9b0a92389dd
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-08 15:02:54 -0500

    Add project files.

---
+++ CSharpFundamentalsSimpleClasses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpFundamentalsSimpleClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
